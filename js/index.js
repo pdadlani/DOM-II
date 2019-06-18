740fdb29eadbb3daa099361d3a89dd23b3b11a15
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-06-18 12:49:42 -0700

    added one event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const navLink = document.querySelectorAll('nav a');
+navLink[0].addEventListener('click', function (e) {
+  e.target.style.color = 'hotpink';
+  e.stopPropagation();
+});
+
